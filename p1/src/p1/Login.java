1415f46fbd7016b3c793e10f162bb838c4c14f92
batch299 <krishnamohanvenkat9@gmail.com>
2019-07-18 20:08:17 +0530

    updated new login.java

---
+++ p1/src/p1/Login.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Login
+{
+ public void open()
+ {
+	 //code to open the url
+ }
+ public void login()
+ {
+	 //code for login to enter username, pwd and click sigin
+ }
+}
